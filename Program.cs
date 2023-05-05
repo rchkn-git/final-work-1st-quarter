bf175b7cd0584d17e7794670c7722e8ec57dc021
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-05-05 19:58:17 +0300

    Building program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите длину массива: ");
+int N = int.Parse(Console.ReadLine()!);
+
+string[] ArrSTR = 
+
+string[] GetArray(int n)
+{
+    string[] res = new sting[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        Cosole.WriteLine("Введите " + (i+1) + " строку");
+        res[i] = ;
+    }
+    return res;
+}
