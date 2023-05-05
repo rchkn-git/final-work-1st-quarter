6b9f65a2c5721e0095f43eeee5c9cd1fda72c9ab
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-05-05 20:28:12 +0300

    Program done, need some corrects

--- Program.cs
+++ Program.cs
@@ -1,16 +1,48 @@
 ﻿Console.Write("Введите длину массива: ");
 int N = int.Parse(Console.ReadLine()!);
 
-string[] ArrSTR = 
+string[] ArrSTR = GetStrArray(N);
+string[] FinalArray = new string[0];
+FinalArray = FillFinalArray(ArrSTR, FinalArray);
 
-string[] GetArray(int n)
+Console.WriteLine("Первый массив: ");
+PrintArray(ArrSTR);
+Console.WriteLine();
+Console.WriteLine("Итоговый массив: ");
+PrintArray(FinalArray);
+Console.WriteLine();
+
+string[] GetStrArray(int n)
 {
-    string[] res = new sting[n];
+    string[] res = new string[n];
 
     for (int i = 0; i < n; i++)
     {
-        Cosole.WriteLine("Введите " + (i+1) + " строку");
-        res[i] = ;
+        Console.WriteLine("Введите " + (i+1) + " строку");
+        res[i] = Console.ReadLine();
     }
     return res;
+}
+
+string[] FillFinalArray(string[] array, string[] finalArray)
+{
+    int j = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (array[i].Length <=3)
+        {
+            Array.Resize(ref finalArray, finalArray.Length + 1);
+            finalArray[j] = array[i];
+            j++;
+        } 
+    }
+    return finalArray;    
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
 }
